5a0bb319b6c8f5ad15f990d816b0c499efca77b1
Thomas Fuchs <thomas@fesch.at>
2014-04-01 09:01:19 -0400

    Add index and updater script

---
+++ update_index.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "<!DOCTYPE html><html><body><h1><a href=http://github.com/madrobby/gifs>madrobby/gifs</a></h1>" > index.html
+ls -1pa | grep .gif$ | awk '{ printf "<a href=\"%s\"><img src=\"%s\"></a>\n",$1,$1,$1 }' >> index.html
+echo "</body></html>" >> index.html
